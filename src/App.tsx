125ca1b835359c792a6bd74eee5f5e5f2de9ba19
theVoogie <vugar.abdulzada@webstep.no>
2021-03-23 16:39:23 +0100

    Added router and stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import {Header, Footer, Main} from './components';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import {Header, Footer, CabinsContainer} from './components';
 
 function App() {
+  
+  const slugs = [
+    '/cabins',
+    '/users',
+    '/reservations'
+  ]; 
 
   return (
-    <>
-      <Header/>
-      <Main />
-      <Footer />
-    </>
+      <Router>
+        <Header slugs={slugs}/>
+        <Switch>
+            <Route path={slugs[0]}>
+              <CabinsContainer />
+            </Route>
+            <Route path={slugs[1]}>
+              <h1>Users</h1>
+            </Route>
+            <Route path={slugs[2]}>
+              <h1>Reservations</h1>
+            </Route>
+            
+            <Route path="/*" render={() => <Redirect to={slugs[0]} />} />
+          </Switch>
+        <Footer />
+      </Router>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Header:React.FC = () => {
-  return <header>HYTTARADAR</header> 
-} 
+const Header:React.FC<{ slugs?: string[] }> = ({ slugs }) => {
+
+  return (
+    <header>
+      <div className="hero-title">HYTTARADAR</div>
+      {slugs && slugs.length > 0 && 
+        <ul className="hero-slugs">
+          { slugs.map(slug => <Link to={slug} className="active-slug" key={slug}>{slug}</Link>) }
+        </ul>
+      }
+    </header>
+)}; 
 
 export default Header;

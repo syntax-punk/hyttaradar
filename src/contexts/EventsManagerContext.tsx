412a1923538ae7ed0d9e26f54d5df503839036a6
theVoogie <vugar.abdulzada@webstep.no>
2021-04-05 13:19:42 +0200

    Updated the context

--- src/contexts/EventsManagerContext.tsx
+++ src/contexts/EventsManagerContext.tsx
@@ -9,16 +9,18 @@ const EventsManagerContext = createContext<EventsManagerContextInterface>({
     eventsManager: null
 });
 
-export const useEventsManager = () => {
+export const useEventsManager = (): EventsManagerContextInterface => {
     return useContext(EventsManagerContext);
 };
 
-export function useEventSubscriber<T>(keys: Array<string | CustomEvents>, handler: (message: T) => void) {
+export function useEventSubscriber<T>(keys: Array<string | CustomEvents>, handler: (message: T) => void): CustomEventHub | null {
     const { eventsManager } = useEventsManager();
 
     useEffect(() => { 
         return eventsManager?.subscribe<T>(keys, handler) 
     }, [keys, handler, eventsManager]);
+
+    return eventsManager;
 };
 
 export default EventsManagerContext;

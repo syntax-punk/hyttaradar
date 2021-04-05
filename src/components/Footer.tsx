7937cd7bc02fa44e24eca6129732611563be3664
theVoogie <vugar.abdulzada@webstep.no>
2021-04-05 12:31:40 +0200

    Testing event generator

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useEventsManager } from '../contexts/EventsManagerContext'
 import '../styles/common.scss'
 
 const Footer:React.FC = () => {
-  return <footer>{"made with <3"}</footer> 
+  const { eventsManager } = useEventsManager();
+  const generateEvent = () => {
+    eventsManager?.emit("warning", "This is the shit that happened")
+  };
+
+  return  <footer>
+            {"made with <3"}
+            <button onClick={generateEvent}>Alarm</button>
+          </footer> 
 } 
 
 export default Footer;
